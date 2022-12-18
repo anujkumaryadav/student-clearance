437df2fcf058ebdd2ee41d601590083c3760270e
Anujkumar Yadav <anujkumaryadav.in@gmail.com>
2022-12-18 11:52:41 +0530

    anujkumar/Initial Commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+<style type="text/css">
+<!--
+.style1 {
+	color: #000000;
+	font-weight: bold;
+}
+-->
+</style>
+ <div class="footer">
+          <div class="style1"> <p> Copyright @ <?php echo date("Y")?>. Bajaj Institute of Technology, Wardha | Online Student Clearance System | All Rights Reserved. </p>
+</div>
+</div>
---
+++ sidebar.php
@@ -0,0 +1,15 @@
+<li class="nav-header">
+                </li>
+                <li class="active">
+                <a href="index.php"><i class="fa fa-th-large"></i> <span class="nav-label">Dashboard</span></a></li>
+				<li>
+                    <a href="#"><i class="fa fa-user"></i> <span class="nav-label">Account</span><span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level collapse">
+                 <li><a href="edit-photo.php">Edit Photo</a></li>
+				  <li><a href="changepassword.php">Change Password</a></li>
+
+                    </ul>
+                </li>
+			
+
+			    
