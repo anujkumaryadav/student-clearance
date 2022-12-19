8ee07ced21a7ec5c89a11335e91cebb710a79a37
Prem Mungle <mungleprem@gmail.com>
2022-12-19 22:46:16 +0530

    latter

--- footer.php
+++ footer.php
@@ -1,12 +1,18 @@
 <style type="text/css">
-<!--
+
 .style1 {
 	color: #000000;
 	font-weight: bold;
+	margin-bottom: 1rem;
+	margin-top: 0;
+	text-align:center;
 }
--->
+.copyright{
+	
+}
+
 </style>
  <div class="footer">
-          <div class="style1"> <p> Copyright @ <?php echo date("Y")?>. Bajaj Institute of Technology, Wardha | Online Student Clearance System | All Rights Reserved. </p>
+          <div class="style1" > <p class="copyright"> Copyright @ <?php echo date("Y")?>. Bajaj Institute of Technology, Wardha | Online Student Clearance System | All Rights Reserved. </p>
 </div>
 </div>
